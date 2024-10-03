5a52378c475d1fc9f5729633741a459342965e8f
b0gavmar <b0gavmar@vasvari.org>
2024-10-03 12:16:11 +0200

    commit

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import bodyParser from 'body-parser'
 import express from 'express'
 
-import {roueter as adminRoutes} from './routes/admin.js' //beepitett moduloknal nem kell, amit mi csinlaunk ott kelll js
+        //{roueter as adminRoutes}
+import adminRoutes from './routes/admin.js' //beepitett moduloknal nem kell, amit mi csinlaunk ott kelll js
+import shopRoutes from './routes.shop.js'
 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/admin',adminRoutes);
+app.use('/', shopRoutes);
 
 app.listen(PORT, () => console.log(`server runs on https://localhost:` + PORT))
