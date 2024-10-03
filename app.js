059a660f00deb181692c803909f06fe77232e33f
b0gavmar <b0gavmar@vasvari.org>
2024-10-03 12:29:13 +0200

    .

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 import bodyParser from 'body-parser'
 import express from 'express'
+import path from 'path';
 
         //{roueter as adminRoutes}
 import adminRoutes from './routes/admin.js' //beepitett moduloknal nem kell, amit mi csinlaunk ott kelll js
-import shopRoutes from './routes.shop.js'
+import shopRoutes from './routes/shop.js'
+import __dirname from './util/rootpath.js'
 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(path.join(__dirname,'public')));
 app.use('/admin',adminRoutes);
 app.use('/', shopRoutes);
 
-app.listen(PORT, () => console.log(`server runs on https://localhost:` + PORT))
+app.listen(PORT, () => console.log(`server runs on http://localhost:${PORT}`))
